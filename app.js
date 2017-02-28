3a000148dea6815853f0141d152b0c2c424f1c90
root <quentin.cly@outlook.com>
2017-02-28 12:48:32 +0100

    ajout fichiers

---
+++ app.js
@@ -0,0 +1,36 @@
+var log = function log (data) {
+    
+    "use strict";
+    window.console.log(data);
+}
+
+
+
+
+var calculatrice = function calculatrice() {
+    
+    "use strict";
+   
+    var btn, saisie, add, sub, mult, div; 
+     btn = document.getElementById("btn");
+    saisie = document.getElementById("ma_saisie");
+    log(btn);
+    log(saisie);
+    
+    
+
+function gererClicks() {
+    log("click 2 !!!");
+    log(saisie.value);
+    var res = add(10,10);
+    log(res);
+    }
+    
+    function.add(a, b){
+        return a + b;
+    }
+
+btn.onclick = gererClicks;
+
+//btn.addEventListener("click", gererClicks);
+}
