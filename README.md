e1924a1dffc70e1b44ca8761b891a635f35e37fa
root <quentin.cly@outlook.com>
2017-03-02 11:25:19 +0100

    premier commit git status !

---
+++ README.md
@@ -0,0 +1,3 @@
+#calculatrice
+# calculatrice
+#calculatrice
---
+++ entrainement1/app.js
@@ -0,0 +1,104 @@
+var log = function log (val) 
+{  "use strict";
+ window.console.log(val);};
+
+var afficherInfosUtilisateur = function afficherInfoUtilisateur(user) {...}
+
+
+var comprendreReturn = function conprendreReturn(a1) {
+    "use strict";
+   return typeof a1;
+}
+
+log(comprendreReturn("un exemple..."));
+res = comprendreReturn (true && true && false);
+log(res);
+                                                                       
+res = comprendreReturn({name: "Jack"});
+                                                                       
+if (res === "object")
+
+
+var entrainement1 = function entrainement1() {
+    "use strict";
+var str, bool, nb, und, isnull, obj;{
+ 
+ 
+ str = "une chaine de caracs";
+ nb = window.innerWidth;
+ bool = typeof str === "string";
+ und = "une chaine de caracs";
+ isnull = document.getElementById("vide");
+ 
+ 
+ obj = {};
+ 
+ 
+ 
+ document.write("la valeur de str = " + str + '<br>');
+ document.write("la valeur de bool = " + bool + '<br>');
+ document.write("la valeur de nb = " + nb + '<br>');
+ document.write("la valeur de und = " + und + '<br>');
+ document.write("la valeur de isnull = " + isnull + '<br>');
+ document.write("la valeur de obj = " + obj + '<br>');
+ 
+ 
+ 
+ obj.estPratique = true;
+ obj.estMultiforme = true;
+ obj.estDifficile = false;
+ 
+ 
+ 
+ document.write("les objets sont pratique ? " + obj.estPratique + "" + obj.estPratique + "<br> <br>");
+ 
+ 
+ 
+ 
+ 
+ 
+ var utilisateur1 = {
+     nom: "amangoula",
+     prenom: "guillaume",
+     age: 36,
+     profession: "dev js",
+     couleurPref: "noir"  }
+ 
+ 
+ var utilisateur2 = {
+     nom: "Guer",
+     prenom: "Sef",
+     age: 32,
+     profession: "dev html",
+     couleurPref: "vert"  } 
+
+ 
+ /* //////////////// UTILISATEUR 1 ///////////// */
+ 
+document.write("l'utilisateur 1 se nomme : " + utilisateur1.nom + "" + utilisateur1.prenom + "<br>");
+    
+document.write("l'utilisateur 1 est age de : " + utilisateur1.age + "" + "<br>");
+    
+    document.write("l'utilisateur 1 est  : " + utilisateur1.profession + "" + "<br>");
+    
+    document.write("la couleur prefere de l'utilisateur 1 est : " + utilisateur1.couleurPref + "" + "<br> <br>");
+
+
+
+    
+    /* //////////  UTILISATEUR 2 /////////////*/
+    
+    document.write("l'utilisateur 2 se nomme : " + utilisateur2.nom + "" + utilisateur2.prenom + "<br>");
+    document.write("l'utilisateur 2 est age de : " + utilisateur2.age + "" + "<br>");
+    
+    document.write("l'utilisateur 2 est  : " + utilisateur2.profession + "" + "<br>");
+    
+    document.write("la couleur prefere de l'utilisateur 2 est : " + utilisateur2.couleurPref + "" + "<br>");
+     };
+
+    
+    var utilisateur (...);
+    var utilisateur (...);
+    
+    
+    if(" Utilisateur1 : " + utilisateur2.nom + "" + utilisateur2.prenom + "<br>");
